7f64370475edfd3e41026346f4828ce07f4515bf
vasvenk <vas.venk@berkeley.edu>
2018-11-13 18:20:34 -0800

    Omg learn to commit boi

--- proj/content.js
+++ proj/content.js
@@ -2,17 +2,11 @@ var x = document.getElementById('atfResults').getElementsByClassName("s-result-i
 for (var i = 0, l = x.length; i < l; i++) {
   console.log(x[i])
   var img = document.createElement('img');
-  img.src = 'https://raw.githubusercontent.com/vasvenk/purchoose/master/images/logo%20options/1x/logo_green(48x48).png?token=AYycbPlT9kBGuLTZW6BtfHlTa5b8hC_rks5b6f_CwA%3D%3D';
-  x[i].firstChild.appendChild(img);
+  img.src = 'https://raw.githubusercontent.com/vasvenk/purchoose/master/images/logo%20options/1x/logo_orange(48x48).png';
+  linebreak = document.createElement("br");
+  var newDiv = document.createElement('div');
+  newDiv.appendChild(linebreak)
+  newDiv.innerHTML = '<span class="a-declarative" data-action="a-popover" data-a-popover="{"max-width":"700","closeButton":"false","position":"triggerBottom"}"\n  <a href="javascript:void(0)" class="a-popover-trigger a-declarative">\n    <i class="a-icon a-icon-popover"></i>\n  </a>\n</span>';
+  newDiv.appendChild(img)
+  x[i].firstChild.appendChild(newDiv);
 }
-
-chrome.runtime.onMessage.addListener( function(request, sender, sendResponse) {
-    console.log("something happening from the extension");
-    var data = request.data || {};
-
-    var linksList = document.querySelectorAll('a');
-    [].forEach.call(linksList, function(header) {
-        header.innerHTML = request.data;
-    });
-    sendResponse({data: data, success: true});
-});
